5b08ea9455efe88d16deb5489be57126e02b0a5c
deependra227 <cdeep@iitk.ac.in>
2020-06-22 15:53:43 +0530

    App Deep Linking done & minor changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,78 +1,216 @@
 import 'dart:async';
+import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:uni_links/uni_links.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(new MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        brightness: Brightness.dark,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+  _MyAppState createState() => new _MyAppState();
+}
+
+enum UniLinksType { string, uri }
+
+class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
+  String _latestLink = 'Unknown';
+  Uri _latestUri;
+
+  StreamSubscription _sub;
+
+  UniLinksType _type = UniLinksType.string;
+
+  @override
+  initState() {
+    super.initState();
+    setState(() {
+      isLoaded = false;
+    });
+    initPlatformState();
   }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
+  dispose() {
+    if (_sub != null) _sub.cancel();
+    super.dispose();
+  }
 
-class _MyHomePageState extends State<MyHomePage> {
+  // Platform messages are asynchronous, so we initialize in an async method.
+  initPlatformState() async {
+    if (_type == UniLinksType.string) {
+      await initPlatformStateForStringUniLinks();
+    } else {
+      await initPlatformStateForUriUniLinks();
+    }
+  }
+
+  /// An implementation using a [String] link
+  initPlatformStateForStringUniLinks() async {
+    // Attach a listener to the links stream
+    _sub = getLinksStream().listen((String link) {
+      if (!mounted) return;
+      setState(() {
+        _latestLink = link ?? 'Unknown';
+        _latestUri = null;
+        try {
+          if (link != null) _latestUri = Uri.parse(link);
+        } on FormatException {}
+      });
+    }, onError: (err) {
+      if (!mounted) return;
+      setState(() {
+        _latestLink = 'Failed to get latest link: $err.';
+        _latestUri = null;
+      });
+    });
+
+    // Attach a second listener to the stream
+    getLinksStream().listen((String link) {
+      print('got link: $link');
+    }, onError: (err) {
+      print('got err: $err');
+    });
+
+    // Get the latest link
+    String initialLink;
+    Uri initialUri;
+    // Platform messages may fail, so we use a try/catch PlatformException.
+    try {
+      initialLink = await getInitialLink();
+      print('initial link: $initialLink');
+      if (initialLink != null) initialUri = Uri.parse(initialLink);
+    } on PlatformException {
+      initialLink = 'Failed to get initial link.';
+      initialUri = null;
+    } on FormatException {
+      initialLink = 'Failed to parse the initial link as Uri.';
+      initialUri = null;
+    }
+
+    // If the widget was removed from the tree while the asynchronous platform
+    // message was in flight, we want to discard the reply rather than calling
+    // setState to update our non-existent appearance.
+    if (!mounted) return;
+
+    setState(() {
+      _latestLink = initialLink;
+      _latestUri = initialUri;
+    });
+  }
+
+  /// An implementation using the [Uri] convenience helpers
+  initPlatformStateForUriUniLinks() async {
+    // Attach a listener to the Uri links stream
+    _sub = getUriLinksStream().listen((Uri uri) {
+      if (!mounted) return;
+      setState(() {
+        _latestUri = uri;
+        _latestLink = uri?.toString() ?? 'Unknown';
+      });
+    }, onError: (err) {
+      if (!mounted) return;
+      setState(() {
+        _latestUri = null;
+        _latestLink = 'Failed to get latest link: $err.';
+      });
+    });
+
+    // Attach a second listener to the stream
+    getUriLinksStream().listen((Uri uri) {
+      print('got uri: ${uri?.path} ${uri?.queryParametersAll}');
+    }, onError: (err) {
+      print('got err: $err');
+    });
+
+    // Get the latest Uri
+    Uri initialUri;
+    String initialLink;
+    // Platform messages may fail, so we use a try/catch PlatformException.
+    try {
+      initialUri = await getInitialUri();
+      print('initial uri: ${initialUri?.path}'
+          ' ${initialUri?.queryParametersAll}');
+      initialLink = initialUri?.toString();
+    } on PlatformException {
+      initialUri = null;
+      initialLink = 'Failed to get initial uri.';
+    } on FormatException {
+      initialUri = null;
+      initialLink = 'Bad parse the initial link as Uri.';
+    }
+
+    // If the widget was removed from the tree while the asynchronous platform
+    // message was in flight, we want to discard the reply rather than calling
+    // setState to update our non-existent appearance.
+    if (!mounted) return;
+
+    setState(() {
+      _latestUri = initialUri;
+      _latestLink = initialLink;
+    });
+  }
+
   var url = "https://www.youtube.com/";
   bool isLoaded;
   final Completer<InAppWebViewController> _controller =
       Completer<InAppWebViewController>();
-
-  @override
-  void initState() {
-    setState(() {
-      isLoaded = false;
-    });
-    super.initState();
-  }
-
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-        child: Stack(
-      children: <Widget>[
-        FutureBuilder<InAppWebViewController>(
-          future: _controller.future,
-          builder: (BuildContext context,
-              AsyncSnapshot<InAppWebViewController> controller) {
-            return _webViewPage(context, controller);
-          },
-        ),
-        (isLoaded == false)
-            ? Scaffold(
-                backgroundColor: Colors.white,
-                body: Center(
-                    child: Image.asset(
-                  'assets/logo.png',
-                  scale: 2,
-                )))
-            : Container()
-      ],
-    ));
+    return new MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: SafeArea(
+            child: Stack(
+          children: <Widget>[
+            FutureBuilder<InAppWebViewController>(
+              future: _controller.future,
+              builder: (BuildContext context,
+                  AsyncSnapshot<InAppWebViewController> controller) {
+                return _webViewPage(context, controller);
+              },
+            ),
+            (isLoaded == false)
+                ? Scaffold(
+                    backgroundColor: Colors.white,
+                    body: Center(
+                        child: Image.asset(
+                      'assets/logo.png',
+                      scale: 2,
+                    )))
+                : Container()
+          ],
+        ))
+        // );
+
+        );
   }
 
   backFunction(BuildContext context,
       AsyncSnapshot<InAppWebViewController> controller) async {
-    if (controller.data.canGoBack() != null) {
+    if (await controller.data.canGoBack())
       controller.data.goBack();
+    else {
+      showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+                title: Text("Do you really want to exit the app"),
+                actions: <Widget>[
+                  FlatButton(
+                    child: Text("No"),
+                    onPressed: () => Navigator.pop(context, false),
+                  ),
+                  FlatButton(
+                    child: Text("Yes"),
+                    onPressed: () => exit(1),
+                  ),
+                ],
+              ));
     }
   }
 
+  InAppWebViewController webView;
   _webViewPage(
       BuildContext context, AsyncSnapshot<InAppWebViewController> controller) {
     return WillPopScope(
@@ -99,11 +237,17 @@ class _MyHomePageState extends State<MyHomePage> {
                 useOnLoadResource: true,
               )),
           onWebViewCreated: (InAppWebViewController controller) {
+            webView = controller;
             _controller.complete(controller);
           },
-          onLoadStart: (context, url) async {},
+          onLoadStart: (context, url) async {
+            print(_latestLink);
+            if (_latestLink != null) webView.loadUrl(url: _latestLink);
+            _latestLink = null;
+          },
           onLoadStop: (context, url) async {
             print(url);
+
             setState(() {
               isLoaded = true;
             });
