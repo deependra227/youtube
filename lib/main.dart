a92b3c855aaa2d8d8f790d9341043c48a69db04b
deependra227 <cdeep@iitk.ac.in>
2020-06-27 20:46:32 +0530

    PIP added

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,26 @@
 import 'dart:async';
-import 'dart:io';
+import 'dart:convert';
+import 'dart:io' as io;
+import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:uni_links/uni_links.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:http/http.dart' as http;
+import 'package:flutter_downloader/flutter_downloader.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:flutter_android_pip/flutter_android_pip.dart';
 
-void main() => runApp(new MyApp());
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await FlutterDownloader.initialize(
+      debug: true // optional: set false to disable printing logs to console
+      );
+  await Permission.storage.request();
+  runApp(new MyApp());
+}
 
 class MyApp extends StatefulWidget {
   @override
@@ -18,7 +32,8 @@ enum UniLinksType { string, uri }
 class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
   String _latestLink = 'Unknown';
   Uri _latestUri;
-
+  List videosList = List();
+  var isfetching = false;
   StreamSubscription _sub;
 
   UniLinksType _type = UniLinksType.string;
@@ -36,6 +51,27 @@ class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
   dispose() {
     if (_sub != null) _sub.cancel();
     super.dispose();
+  }
+
+  _fetchUrl(String _url) async {
+    setState(() {
+      isfetching = true;
+    });
+    _url = _url.split('?')[1];
+    print(_url);
+    var response = await http.post("https://y2mate.guru/api/convert",
+        body: {"url": "https://youtube.com/watch?" + _url});
+    // print(response.body);
+    if (response.statusCode == 200) {
+      List responsejson = json.decode(response.body)['url'] as List;
+      print(responsejson.length);
+      setState(() {
+        isfetching = false;
+      });
+      return responsejson;
+    } else {
+      throw Exception('Failed to fetch');
+    }
   }
 
   // Platform messages are asynchronous, so we initialize in an async method.
@@ -153,7 +189,7 @@ class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
     });
   }
 
-  var url = "https://www.youtube.com/";
+  var url = "https://m.youtube.com/?persist_app=1&app=m";
   bool isLoaded;
   final Completer<InAppWebViewController> _controller =
       Completer<InAppWebViewController>();
@@ -161,6 +197,13 @@ class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
   Widget build(BuildContext context) {
     return new MaterialApp(
         debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          brightness: Brightness.light,
+          primaryColor: Colors.red,
+        ),
+        darkTheme: ThemeData(
+          brightness: Brightness.dark,
+        ),
         home: SafeArea(
             child: Stack(
           children: <Widget>[
@@ -173,13 +216,13 @@ class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
             ),
             (isLoaded == false)
                 ? Scaffold(
-                    backgroundColor: Colors.white,
+                    backgroundColor: Color.fromRGBO(40, 40, 40, 1),
                     body: Center(
                         child: Image.asset(
                       'assets/logo.png',
                       scale: 2,
                     )))
-                : Container()
+                : Container(),
           ],
         ))
         // );
@@ -203,7 +246,7 @@ class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
                   ),
                   FlatButton(
                     child: Text("Yes"),
-                    onPressed: () => exit(1),
+                    onPressed: () => io.exit(1),
                   ),
                 ],
               ));
@@ -211,47 +254,162 @@ class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
   }
 
   InAppWebViewController webView;
+  bool youtubeBar = true;
   _webViewPage(
       BuildContext context, AsyncSnapshot<InAppWebViewController> controller) {
     return WillPopScope(
         onWillPop: () => backFunction(context, controller),
-        child: InAppWebView(
-          initialUrl: url,
-          initialOptions: InAppWebViewGroupOptions(
-              android: AndroidInAppWebViewOptions(
-                builtInZoomControls: false,
-                allowContentAccess: true,
-                allowFileAccess: true,
-                allowFileAccessFromFileURLs: true,
-                allowUniversalAccessFromFileURLs: true,
-                hardwareAcceleration: true,
-                cacheMode: AndroidCacheMode.LOAD_DEFAULT,
-                networkAvailable: true,
-              ),
-              crossPlatform: InAppWebViewOptions(
-                javaScriptEnabled: true,
-                cacheEnabled: true,
-                debuggingEnabled: true,
-                mediaPlaybackRequiresUserGesture: true,
-                javaScriptCanOpenWindowsAutomatically: true,
-                useOnLoadResource: true,
-              )),
-          onWebViewCreated: (InAppWebViewController controller) {
-            webView = controller;
-            _controller.complete(controller);
-          },
-          onLoadStart: (context, url) async {
-            print(_latestLink);
-            if (_latestLink != null) webView.loadUrl(url: _latestLink);
-            _latestLink = null;
-          },
-          onLoadStop: (context, url) async {
-            print(url);
+        child: Scaffold(
+          bottomNavigationBar: (MediaQuery.of(context).size.height > 300)
+              ? BottomAppBar(
+                  shape: CircularNotchedRectangle(),
+                  elevation: 5.0,
+                  color: Color.fromRGBO(16, 16, 16, 1),
+                  child: Padding(
+                    padding: EdgeInsets.fromLTRB(28, 0, 30, 0),
+                    child: new Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        IconButton(
+                          onPressed: () {
+                            webView.loadUrl(url: url);
+                          },
+                          icon: Icon(Icons.home),
+                        ),
+                        IconButton(
+                            icon: Icon(Icons.file_download),
+                            onPressed: () async {
+                              
+                              var _url = await webView.getUrl();
+                              print(_url);
+                              videosList = await _fetchUrl(_url);
+                              webView.loadUrl(url: videosList[0]['url']);
+                              print(videosList);
+                            }),
 
-            setState(() {
-              isLoaded = true;
-            });
-          },
+                        // IconButton(
+                        //   onPressed: () {},
+                        //   icon: Icon(Icons.video_library),
+                        // ),
+                        IconButton(
+                          onPressed: () {
+                            FlutterAndroidPip.enterPictureInPictureMode;
+                            webView.evaluateJavascript(
+                                source:
+                                    " document.getElementsByTagName('ytm-mobile-topbar-renderer')[0].style.display = 'none' ");
+                            webView.evaluateJavascript(
+                                source:
+                                    " document.documentElement.scrollTop = 0;");
+                            webView.evaluateJavascript(
+                                source: " window.scrollBy(0, 50); ");
+                            webView.evaluateJavascript(
+                                source:
+                                    " document.getElementsByTagName('ytm-pivot-bar-renderer')[0].style.display = 'none' ");
+                            youtubeBar = false;
+                          },
+                          icon: Icon(Icons.picture_in_picture_alt),
+                        ),
+                        IconButton(
+                          onPressed: (){
+                            if(youtubeBar){
+                             webView.evaluateJavascript(source: " document.getElementsByTagName('ytm-pivot-bar-renderer')[0].style.display = 'none' ");
+                             setState(() {
+                             youtubeBar = false;
+                               
+                             });
+                             }
+                            else{
+                               webView.evaluateJavascript(source: " document.getElementsByTagName('ytm-pivot-bar-renderer')[0].style.display = '' ");
+                             setState(() {
+                             youtubeBar = true;
+                               
+                             });
+                            }
+                          },
+                          icon: youtubeBar ? Icon(Icons.arrow_drop_down) : Icon(Icons.arrow_drop_up),
+                        )
+                      ],
+                    ),
+                  ),
+                )
+              : BottomAppBar(),
+          body: InAppWebView(
+            initialUrl: url,
+            initialOptions: InAppWebViewGroupOptions(
+                android: AndroidInAppWebViewOptions(
+                  builtInZoomControls: false,
+                  allowContentAccess: true,
+                  allowFileAccess: true,
+                  allowFileAccessFromFileURLs: true,
+                  allowUniversalAccessFromFileURLs: true,
+                  hardwareAcceleration: true,
+                  cacheMode: AndroidCacheMode.LOAD_DEFAULT,
+                  networkAvailable: true,
+                ),
+                crossPlatform: InAppWebViewOptions(
+                  javaScriptEnabled: true,
+                  cacheEnabled: true,
+                  debuggingEnabled: true,
+                  mediaPlaybackRequiresUserGesture: true,
+                  javaScriptCanOpenWindowsAutomatically: true,
+                  useOnLoadResource: true,
+                  useOnDownloadStart: true,
+                )),
+            onWebViewCreated: (InAppWebViewController controller) {
+              webView = controller;
+              _controller.complete(controller);
+            },
+            onLoadStart: (context, url) async {
+              print(_latestLink);
+              if (_latestLink != null) webView.loadUrl(url: _latestLink);
+              _latestLink = null;
+            },
+            onLoadStop: (context, url) async {
+              print(url);
+              setState(() {
+                isLoaded = true;
+              });
+            },
+            onScrollChanged: (controller, int x, int y) {
+              print('X: $x');
+              print('Y: $y');
+              if (y == 0) {
+                webView.evaluateJavascript(
+                    source:
+                        " document.getElementsByTagName('ytm-mobile-topbar-renderer')[0].style.display = '' ");
+                //  webView.evaluateJavascript(source: " window.scrollBy(0, -50); ");
+
+                // webView.evaluateJavascript(
+                //     source:
+                //         " document.getElementsByTagName('ytm-pivot-bar-renderer')[0].style.display = '' ");
+                youtubeBar = true;
+              }
+            },
+            onDownloadStart:(InAppWebViewController controller, String url) async {
+              print("onDownloadStart");
+              var path = (await getExternalStorageDirectory()).path;
+              Uri uri = Uri.dataFromString(url);
+              print(await io.Directory(path).listSync().toList());
+              List dirFile = await io.Directory(path).list().toList();
+              for (int i = 0; i < dirFile.length; i++) {
+                if (dirFile[i].toString().substring(0, dirFile[i].toString().length - 1).split('files/')[1] == uri.queryParameters['video_id']) {
+                  print('haaaaaaaaaaaaaaaa');
+                  return;
+                }
+              }
+              final taskId = await FlutterDownloader.enqueue(
+                fileName: uri.queryParameters['video_id'],
+                url: url,
+                savedDir: path,
+                showNotification:
+                    true, // show download progress in status bar (for Android)
+                openFileFromNotification:
+                    true, // click on notification to open downloaded file (for Android)
+              );
+              // FlutterDownloader.open(taskId: taskId);
+            },
+          ),
         ));
   }
 }
